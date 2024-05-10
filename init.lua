02eee88e0fb6f53510cfae80fd851d3a3351e260
jimmydesu <jimsyyap@gmail.com>
2024-05-11 09:33:48 +1000

    colorscheme to habamax

--- init.lua
+++ init.lua
@@ -142,6 +142,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- this so i can use sorcerer in nvim/colors 12-may2024
+vim.opt.termguicolors = true
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
@@ -788,14 +791,16 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     --'folke/tokyonight.nvim',
-    'catppuccin/nvim',
+    --'catppuccin/nvim',
+    'ntk148v/habamax.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       --vim.cmd.colorscheme 'tokyonight-night'
-      vim.cmd.colorscheme 'catppuccin'
+      --vim.cmd.colorscheme 'catppuccin'
+      vim.cmd.colorscheme 'habamax'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,12 +1,14 @@
 return {
-  -- add catppuccin
-  --{ 'catppuccin/nvim' },
-
-  ---- configure lazyvim to load catppuccin
-  --{
-  --  'LazyVim/LazyVim',
-  --  opts = {
-  --    colorscheme = 'catppuccin',
+  --  {
+  --    'catppuccin',
+  --    opts = {
+  --      transparent_background = true,
+  --    },
   --  },
-  --},
+  --  {
+  --    'LazyVim/LazyVim',
+  --    opts = {
+  --      colorscheme = 'catppuccin',
+  --    },
+  --  },
 }
