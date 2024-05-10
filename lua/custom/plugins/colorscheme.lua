dccf34d47444b0efcc7fff9d2b65f92cf0001c5a
jimmydesu <jimsyyap@gmail.com>
2024-05-11 08:42:37 +1000

    playing sorcerer

---
+++ lua/custom/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add catppuccin
+  --{ 'catppuccin/nvim' },
+
+  ---- configure lazyvim to load catppuccin
+  --{
+  --  'LazyVim/LazyVim',
+  --  opts = {
+  --    colorscheme = 'catppuccin',
+  --  },
+  --},
+}
