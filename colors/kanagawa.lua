a0912fa96f7b2a4cc670afa1e11fbf7d64111161
jimmydesu <jimsyyap@gmail.com>
2024-05-14 07:57:24 +1000

    css-ls added

---
+++ colors/kanagawa.lua
@@ -0,0 +1,8 @@
+return {
+  -- 'rebelot/kanagawa.nvim',
+  -- lazy = true,
+  -- config = function()
+  --   -- load the colorscheme here
+  --   vim.cmd.colorscheme [[ colorscheme kanagawa]]
+  -- end,
+}
--- init.lua
+++ init.lua
@@ -508,10 +508,10 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {},
         -- gopls = {},
         bashls = {},
-        --cssls = {},
+        cssls = {},
         --css_variables = {},
         --emmet_language_server = {},
         emmet_ls = {},
