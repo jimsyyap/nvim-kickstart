a44821a57f34fc6994e1efda4eda0a540b75eccf
jimmydesu <jimsyyap@gmail.com>
2024-05-14 11:43:04 +1000

    delve was added

--- init.lua
+++ init.lua
@@ -508,17 +508,18 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        clangd = {},
+        --clangd = {},
         -- gopls = {},
         bashls = {},
-        cssls = {},
+        --cssls = {},
         --css_variables = {},
         --emmet_language_server = {},
         emmet_ls = {},
-        html = {},
+        --html = {},
+        -- uninstalled html-lsp as snippets adds extra indent? testing
         --jinja_lsp = {},
         jedi_language_server = {}, --this for python lsp
-        jsonls = {},
+        --jsonls = {},
         prettier = {}, -- javascript et al, else tsserver as comment below
         phpactor = {}, -- this for php
         --postgres_lsp = {},
