1ad4d8e106a3dfd7d6a1da94cf17cd99ab7d9f7b
jimmydesu <jimsyyap@gmail.com>
2024-05-18 10:23:24 +1000

    sourcerer colorscheme works

--- init.lua
+++ init.lua
@@ -758,7 +758,8 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     --'folke/tokyonight.nvim',
-    'catppuccin/nvim',
+    --'catppuccin/nvim',
+    'xero/sourcerer.vim',
     --'ntk148v/habamax.nvim',
     --'rebelot/kanagawa.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
@@ -767,7 +768,7 @@ require('lazy').setup({
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       --vim.cmd.colorscheme 'tokyonight-night'
-      vim.cmd.colorscheme 'catppuccin'
+      vim.cmd.colorscheme 'sourcerer'
       --vim.cmd.colorscheme 'kanagawa-wave'
       --vim.cmd.colorscheme 'habamax'
 
