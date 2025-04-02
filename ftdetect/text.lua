7f4a5f36666e1325d60925088f2e49c953eeb65e
jimmy desu <jimsyyap@gmail.com>
2025-04-02 15:26:50 +1100

    added text.lua on ftdetect is joy

---
+++ ftdetect/text.lua
@@ -0,0 +1,12 @@
+-- this for text editing stuff vim cmds
+return {
+	vim.cmd([[
+    for char in split('abcdefghijklmnopqrstuvwxyz', '\zs')
+        exe printf("inoremap <expr> %s search('[.!?]\\_s\\+\\%%#', 'bcnw') ? '%s' : '%s'", char, toupper(char), char)
+    endfor
+
+    for char in split('ABCDEFGHIJKLMNOPQRSTUVWXYZ', '\zs')
+        exe printf("inoremap <expr> %s search('[.!?]\\_s\\+\\%%#', 'bcnW') ? '%s' : '%s'", char, tolower(char), char)
+    endfor
+    ]]),
+}
